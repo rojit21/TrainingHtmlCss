bb524de86b034918df1103e36f0573d6619e1a1e
rojit21 <luciferden98@gmail.com>
2025-06-17 15:00:14 +0545

    java script

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+ /**console.log("Welcome to the Javascript");
+let name="Rojit";
+let age="19"
+let isojtover="false";
+let colors = ["red", "blue", "green"];
+console.log(" the name is "+name);
+console.log("the age is "+age);
+console.log(isojtover);
+console.log(colors);
+
+
+let a;
+console.log(a);
+a=3;
+console.log(a);
+**/
+
+let age=10;
+if(age>18)
+{
+    console.log("U are voter u can vote now ");
+}
+else{
+    console.log("u are nablik  grow fastly  mf . . .  ");
+}
