cec7d0922439899a72bc6798b4b1e3c39faef884
rojit21 <luciferden98@gmail.com>
2025-06-20 13:43:43 +0545

    javascript 3

---
+++ javascript3/app.js
@@ -0,0 +1,48 @@
+
+let age=19;
+if(age>18)
+{
+console.log("u are voter ");
+}
+else{
+    console.log("u are non voter");
+}
+
+let number=60;
+if(number>90)
+{
+    console.log("A+")
+}
+else if(number > 50)
+{
+    console.log("B");
+}
+else {
+    console.log("fail");
+}
+
+let username="admin";
+let password="rojit";
+if(username=="admin" && password=="rojit")
+{
+    console.log("congrats bc u login sucessfully");
+}
+else{
+    console.log("error try again");
+}
+
+let number1=10;
+let number2=20;
+if(number1||number2==20)
+{
+    console.log("one of the number is 20 or correct");
+}
+
+let number3=10;
+if(number3!=10)
+{
+    console.log("number is  not equal to 10");
+}
+else{
+    console.log("number is  equal to 10");
+}
