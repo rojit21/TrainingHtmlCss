4808a21b408608ee70de852ebbd3be46cf5cec22
rojit21 <luciferden98@gmail.com>
2025-06-20 13:42:59 +0545

    javascript day1

---
+++ javascript1/hello.js
@@ -0,0 +1,68 @@
+ /**basic structure of javascript
+ console.log("Welcome to the Javascript"); */
+
+ /**javascript datatypes 
+let name="Rojit"; // string
+let age=19; // number or integer
+let isojtover="false"; // boleean 
+let colors = ["red", "blue", "green"]; // array
+const person = {firstName:"rojit", lastName:"acharya"};
+
+console.log(" the name is "+name); 
+console.log("the age is "+age);
+console.log(isojtover);
+console.log(colors); 
+console.log(person); 
+*/ 
+
+/** declare a simple variable  
+let a;
+console.log(a);
+a=3;
+console.log(a);
+*/
+
+
+
+/**conditonal statement 
+let age=10;
+if(age>18)
+{
+    console.log("U are voter u can vote now ");
+}
+else{
+    console.log("u are nablik  grow fastly  mf . . .  ");
+}
+*/ 
+ /** var declare method   --> reassignable 
+                        --> redeclarable
+var name="anmol";
+var name="rojit";
+name="mohan";
+console.log(name);
+
+let declare method  --> reassignable 
+let age=20;
+age=25;
+console.log(age);
+
+ const declare method --> not redeclarable
+                      --> not reassignable 
+ const age=19;
+ console.log(age);
+ */ 
+
+let name="Rojit"; // string
+let age=19; // number or integer
+let isojtover="false"; // boleean 
+let colors = ["red", "blue", "green"]; // array
+const person = {firstName:"rojit", lastName:"acharya"};
+
+console.log(" the name is "+name); 
+console.log("the age is "+age);
+console.log(isojtover);
+console.log(colors); 
+console.log(person); 
+
+
+
