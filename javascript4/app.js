6fb732a30256006581db893059ee55a749e72b21
rojit21 <luciferden98@gmail.com>
2025-06-22 14:24:46 +0545

    javascript4

---
+++ javascript4/app.js
@@ -0,0 +1,28 @@
+/** 
+function myFunction() {
+let x = 10;
+x = x + 5;
+if (x > 10) {
+    console.log("x is greater than 10 and its value is: " + x);
+}
+}
+console.log("Hello World");
+myFunction();
+**/
+
+function calculatorfunction()
+{
+    let a = 45;
+    let b = 10;
+    let sum = a + b;
+    let mul = a * b;
+    let sub = a - b;
+    let div = a / b;
+    console.log("The multiplication is: " + mul);
+    console.log("The sum is: " + sum);
+    console.log("The subtraction is: " + sub);
+    console.log("The division is: " + div);
+}
+calculatorfunction();
+
+
