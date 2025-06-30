0a30724d8001740befed9612d5893b176c389210
rojit21 <luciferden98@gmail.com>
2025-06-30 13:52:28 +0545

    day10

--- javascript10/app.js
+++ javascript10/app.js
@@ -16,15 +16,26 @@
 // Online orders
 // Exams (pass/fail)
 
-let food_deliery = new Promise(function (resolve, reject) {
-    let deliver = true;
-    if (deliver) {
-        resolve("food deliver sucessfully")
-    }
-    else {
-        reject("pending plz wait ")
-    }
-});
-food_deliery
-    .then((msg) => console.log(msg))
-    .catch((err) => console.log(err));
+// let food_deliery = new Promise(function (resolve, reject) {
+//     let deliver = true;
+//     if (deliver) {
+//         resolve("food deliver sucessfully")
+//     }
+//     else {
+//         reject("pending plz wait ")
+//     }
+// });
+// food_deliery
+//     .then((msg) => console.log(msg))
+//     .catch((err) => console.log(err));
+
+console.log('start');
+setTimeout(() => {
+    console.log('iut will log after 2 seconds')
+
+}, 2000);
+setTimeout(() => {
+    console.log('iut will log after 2 secondsaaa')
+
+}, 2000);
+console.log('end');
