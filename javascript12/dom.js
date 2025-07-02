56ee44b1fe25428cab0585c01b0c683393a24849
rojit21 <luciferden98@gmail.com>
2025-07-02 14:47:28 +0545

    day 12 js

---
+++ javascript12/dom.js
@@ -0,0 +1,25 @@
+// const cart = ["apple", "banana", "orange"];
+// api.createorder(cart, function () {
+//     api.proceedPayment(function () {
+//         api.showorderSummary(
+//             function () {
+//                 api.updatewallet()
+//             }
+//         )
+//     })
+// })
+
+
+const api = [
+    {
+        title : "Order Processing API",
+        description : "This API handles the order processing flow including creating an order, proceeding with payment, and showing order summary."
+    },
+    {
+        title : "createorder",
+        description : "This function creates an order with the provided cart items. It takes a callback "
+    },
+];
+
+
+    
